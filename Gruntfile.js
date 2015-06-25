adf51e71414dba9afce3fcc0ecf1e0415e29e3a8
jshemas <jshemas@hotmail.com>
2015-06-25 09:54:17 -0400

    adding grunt to the project!

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = function (grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		// linter
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			appFiles: {
+				src: [
+					'server/*.js',
+					'spec/*.js',
+					'Gruntfile.js',
+					'app.js',
+					'package.json'
+				]
+			}
+		},
+		// linter
+		jscs: {
+			options: {
+				config: '.jscsrc'
+			},
+			appFiles: {
+				src: [
+					'server/*.js',
+					'spec/*.js',
+					'Gruntfile.js',
+					'app.js',
+					'package.json'
+				]
+			}
+		},
+		// mocha tests (server)
+		mochaTest: {
+			options: {
+				reporter: 'progress',
+				bail: true
+			},
+			testApp: {
+				src: 'spec/index.spec.js'
+			}
+		},
+		// express deploy
+		express: {
+			options: {
+				port: 8080
+			},
+			dev: {
+				options: {
+					script: './app.js',
+					node_env: 'dev',
+					nospawn: true,
+					delay: 5
+				}
+			}
+		},
+		// live watcher for file changes
+		watch: {
+			appFile: {
+				files: [
+					'server/*.js',
+					'spec/*.js',
+					'Gruntfile.js',
+					'app.js',
+					'package.json'
+				],
+				tasks: [
+					'express:dev:stop',
+					'jshint:appFiles',
+					'jscs:appFiles',
+					'mochaTest:testApp',
+					'express:dev:start'
+				]
+			}
+		}
+	});
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-jscs');
+	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-express-server');
+	// Tasks.
+	grunt.registerTask('default', [
+		'jshint',
+		'jscs',
+		'watch'
+	]);
+};
