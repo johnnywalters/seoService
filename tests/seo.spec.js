253d1726951f94d382fb6e79c17d08a9e8a86af4
Josh Shemas <joshshemas@gmail.com>
2015-07-08 14:25:11 -0400

    adding another website for tests

--- tests/seo.spec.js
+++ tests/seo.spec.js
@@ -4,7 +4,7 @@ var request = require('supertest'),
 	Expect = require('expect.js');
 
 describe('test getInfo:', function () {
-	it('Valid URL', function (done) {
+	it('Valid URL One', function (done) {
 		var data = {'options': {'url': 'https://www.python.org/', 'keyword': 'Python'}};
 		supertest.post('/getInfo').send(data).end(function (err, result) {
 			Expect(result.res.statusCode).to.be(200);
@@ -30,6 +30,36 @@ describe('test getInfo:', function () {
 			Expect(result.body.result.headerTags.h2.content).to.be('Get Started');
 			Expect(result.body.result.headerTags.h3.status).to.be(0);
 			Expect(result.body.result.headerTags.h3.message).to.be('There is no text in the first H3 on your page');
+			Expect(result.body.result.headerTags.info.doHeaderingsExist).to.be(true);
+			Expect(result.body.result.headerTags.info.headerDuplicate).to.be(false);
+			done();
+		});
+	});
+	it('Valid URL Two', function (done) {
+		var data = {'options': {'url': 'https://www.mozilla.org/en-US/firefox/new/', 'keyword': 'browser'}};
+		supertest.post('/getInfo').send(data).end(function (err, result) {
+			Expect(result.res.statusCode).to.be(200);
+			Expect(result.body.success).to.be(true);
+			Expect(result.body.result.description.content).to.be('Download Mozilla Firefox, a free Web browser. Firefox is created by a global non-profit dedicated to putting individuals in control online. Get Firefox today!');
+			Expect(result.body.result.description.status).to.be(1);
+			Expect(result.body.result.description.message).to.be('It\'s good that you have a meta description, but it should be between 140 and 170 characters. Your\'s is at 158 characters');
+			Expect(result.body.result.description.info.stringLength).to.be(158);
+			Expect(result.body.result.description.info.keywordInDescription).to.be(true);
+			Expect(result.body.result.description.info.keywordStartsDescription).to.be(false);
+			Expect(result.body.result.keywords.status).to.be(0);
+			Expect(result.body.result.keywords.message).to.be('Your site needs a meta keywords.');
+			Expect(result.body.result.title.content).to.be('Download Firefox — Free Web Browser — Mozilla');
+			Expect(result.body.result.title.status).to.be(1);
+			Expect(result.body.result.title.message).to.be('Title tag looks good!');
+			Expect(result.body.result.title.info.stringLength).to.be(45);
+			Expect(result.body.result.title.info.keywordInTitle).to.be(false);
+			Expect(result.body.result.title.info.keywordStartsTitle).to.be(false);
+			Expect(result.body.result.headerTags.h1.status).to.be(1);
+			Expect(result.body.result.headerTags.h1.content).to.be('When it’s personal,choose Firefox.');
+			Expect(result.body.result.headerTags.h2.status).to.be(0);
+			Expect(result.body.result.headerTags.h2.message).to.be('There is no text in the first H2 on your page');
+			Expect(result.body.result.headerTags.h3.status).to.be(1);
+			Expect(result.body.result.headerTags.h3.content).to.be('Get Firefox news');
 			Expect(result.body.result.headerTags.info.doHeaderingsExist).to.be(true);
 			Expect(result.body.result.headerTags.info.headerDuplicate).to.be(false);
 			done();
