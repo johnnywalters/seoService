693dcda70772f30098e78a5f52bdc144af66ee94
Josh Shemas <joshshemas@gmail.com>
2015-07-29 21:21:17 -0400

    adding more tests

--- server/seo.js
+++ server/seo.js
@@ -206,9 +206,9 @@ module.exports = {
 			headerTagObj.h3.status = 0;
 			headerTagObj.h3.message = 'There is no text in the first H3 on your page';
 		}
-		headerTagObj.info.doHeadersExist = false;
+		headerTagObj.info.doAllHeadersExist = false;
 		if (headerTagObj.h1.status === 1 && headerTagObj.h2.status === 1 && headerTagObj.h3.status === 1) {
-			headerTagObj.info.doHeadersExist = true;
+			headerTagObj.info.doAllHeadersExist = true;
 		}
 		headerTagObj.info.headerDuplicate = false;
 		if ((headerTagObj.h1.content && headerTagObj.h2.content && headerTagObj.h1.content === headerTagObj.h2.content) || (headerTagObj.h1.content && headerTagObj.h3.content && headerTagObj.h1.content === headerTagObj.h3.content) || (headerTagObj.h2.content && headerTagObj.h3.content && headerTagObj.h2.content === headerTagObj.h3.content)) {
